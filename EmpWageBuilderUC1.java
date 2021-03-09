72f4cc42edab9ff4a4ea64c67a9c38a4d1f21607
MalavikaAjit <malavikaajit9@gmail.com>
2021-03-09 12:02:33 +0530

    ADD. added feature working hours and working days

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -3,14 +3,15 @@ public class EmpWageBuilderUC1 {
         public static final int IS_PART_TIME = 1;
         public static final int EMP_RATE_PER_HOUR =20;
         public static final int NUM_OF_WORKING_DAYS =20;
-
+        public static final int MAX_HRS_IN_MONTH =100;
        
         public static void main (String[] args) {
         
-        int empHrs = 0, empWage = 0, totalEmpWage = 0;
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 
-        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
-
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) { 
+                
+            totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
                case IS_PART_TIME:
@@ -23,12 +24,12 @@ public class EmpWageBuilderUC1 {
                  empHrs =0;
             }
          
-            empWage = empHrs * EMP_RATE_PER_HOUR;  
-            totalEmpWage += empWage;  
-            System.out.println("Emp Wage:" + empWage );
+            totalEmpHrs  += empHrs;   
+            System.out.println("Day#: " + totalWorkingDays + " Emp Hours " + empHrs);
     
        }
+         int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
          System.out.println("Total emp wage: " + totalEmpWage);
-    } 
+   }
 
 }
